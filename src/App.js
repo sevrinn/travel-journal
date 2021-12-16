2fe1f0afe64f3a34d965c5bd2f7200cdd40ffaf4
Sevrinn <sevrinnsevrinn@gmail.com>
2021-12-16 13:46:17 -0800

    added images and css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import './App.css';
 import Nav from './components/Nav.js';
 import Entry from './components/Entry.js';
+import journalData from './journal-entries'
+
+
+
+
+
 
 function App() {
+
+  const entryElements = journalData.map(data => {
+    return (
+      <Entry 
+        country={data.country} 
+        location={data.location} 
+        googleMapsLink={data.googleMapsLink} 
+        startDate={data.startDate} 
+        endDate={data.endDate} 
+        content={data.content}
+        img={data.imageUrl}
+        altText={data.altText}
+      />
+    )
+   })
+
   return (
     <div className="App">
       <Nav />
-      <Entry />
+      {entryElements}
     </div>
   );
 }
---
+++ src/journal-entries.js
@@ -0,0 +1,39 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+const journalData = [
+  {
+    key: 1,
+    country: "Ireland",
+    location: "County Clare",
+    googleMapsLink: "https://www.google.com/maps/place/Co.+Clare,+Ireland/@52.8611465,-9.6723038,9z/data=!3m1!4b1!4m9!1m2!2m1!1scounty+clare!3m5!1s0x485b11f9c8824e19:0x1800c7acf18001f0!8m2!3d52.8439049!4d-8.9817324!15sCgxjb3VudHkgY2xhcmWSARRhZG1pbmlzdHJhdGl2ZV9hcmVhMQ",
+    startDate: "12 August, 2016",
+    endDate: "5 September, 2016",
+    content: "Clare is a county in western Ireland with terrain ranging from rolling countryside to craggy Atlantic coastline. The Burren, a uniquely rocky wilderness area, shelters Stone Age monuments and ancient churches.",
+    imageUrl: "clare.jpeg",
+    altText: "A castle tower in Ireland overlooking a foggy field",
+  },
+  {
+    key: 2,
+    country: "Portugal",
+    location: "Sintra",
+    googleMapsLink: "https://www.google.com/maps/place/Park+and+National+Palace+of+Pena/@38.7875851,-9.3906089,15z/data=!4m5!3m4!1s0x0:0x77271a7dcb673b58!8m2!3d38.7875851!4d-9.3906089",
+    startDate: "5 April, 2018",
+    endDate: "25 April, 2018",
+    content: "The Pena Palace is a Romanticist castle in São Pedro de Penaferrim, in the municipality of Sintra, on the Portuguese Riviera. The castle stands on the top of a hill in the Sintra Mountains above the town of Sintra, and on a clear day it can be easily seen from Lisbon and much of its metropolitan area.",
+    imageUrl: "pena-palace.jpg",
+    altText: "Pena Palace in Portugal",
+  },
+  {
+    key: 3,
+    country: "Thailand",
+    location: "Chiang Rai",
+    googleMapsLink: "https://www.google.com/maps/place/Mueang+Chiang+Rai,+Mueang+Chiang+Rai+District,+Chiang+Rai,+Thailand/@19.4844322,99.0701542,8.29z/data=!4m5!3m4!1s0x30d7066713c38365:0x711554bbb1b60637!8m2!3d19.9104798!4d99.840576",
+    startDate: "5 Feb, 2009",
+    endDate: "5 March, 2009",
+    content: "Thailand is a Southeast Asian country. It's known for tropical beaches, opulent royal palaces, ancient ruins and ornate temples displaying figures of Buddha. In Bangkok, the capital, an ultramodern cityscape rises next to quiet canalside communities and the iconic temples of Wat Arun, Wat Pho and the Emerald Buddha Temple (Wat Phra Kaew). ",
+    imageUrl: "thailand.jpg",
+    altText: "Buddah at Wat Rong Seur Ten temple",
+  }
+]
+
+export default journalData;
+
